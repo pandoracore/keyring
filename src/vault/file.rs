3f3ee50e9cdce391d956db549161b5b703e96ba9
Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
2020-06-08 16:31:05 +0200

    Project skeleton

---
+++ src/vault/file.rs
@@ -0,0 +1,22 @@
+// Keyring: private/public key managing service
+// Written in 2020 by
+//     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
+//
+// You should have received a copy of the AGPL License
+// along with this software.
+// If not, see <https://www.gnu.org/licenses/agpl-3.0-standalone.html>.
+
+//! File storage drivers for private key vault
+
+use super::Vault;
+
+#[derive(Clone, Debug, Display)]
+#[display_from(Debug)]
+pub struct FileVault {}
+
+impl Vault for FileVault {}
